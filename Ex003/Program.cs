ca6685849e5f2603d579b17c8ce39c9d249e717b
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-12 23:19:27 +0900

    Part of 3

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,15 +1,35 @@
-﻿int Max (int arg1, int arg2, int arg3)
+﻿int[] array = { 11, 29, 13, 27, 55, 16, 27, 78, 19 };
+
+int n = array.Length;
+
+int find = 27;
+int index = 0;
+
+while (index < n )
 {
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    } 
+    // index = index + 1;
+    index++;
 }
 
-int[] array = {11,29,13,46,55,16,27,78,19};
 
-int max = Max (Max(array[0], array[1], array[2]),
-Max(array[3], array[4], array[5]),
-Max(array[6], array[7], array[8]));
 
-Console.WriteLine(max);
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+
+// int max = Max(Max(array[0], array[1], array[2]),
+// Max(array[3], array[4], array[5]),
+// Max(array[6], array[7], array[8]));
+
+// Console.WriteLine(max);
